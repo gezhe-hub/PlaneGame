505c8bf0db8e7b84c319668a71042eaac5cdc3a5
Gaberial <1660146532@qq.com>
2026-03-09 19:20:16 +0800

    necessary objects

--- include/player.h
+++ include/player.h
@@ -1,10 +1,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "raylib.h"
+#define MAX_BULLETS 10
+
+typedef struct {
+  Vector2 pos;
+  float speed;
+  bool active;
+} Bullet;
+
 typedef struct Player {
   Vector2 pos;
   float speed;
   int hp;
+  Bullet bullets[MAX_BULLETS];
 } Player;
 void InitPlayer(Player *p);
 void UpdatePlayer(Player *p);
--- src/player.c
+++ src/player.c
@@ -16,14 +16,40 @@ void UpdatePlayer(Player *p) {
     p->pos.y = 0;
   if (p->pos.y > 800)
     p->pos.y = 800;
+  if (IsKeyPressed(KEY_SPACE)) {
+    for (int i = 0; i < MAX_BULLETS; i++) {
+      if (!p->bullets[i].active) {
+        p->bullets[i].active = true;
+        p->bullets[i].pos = (Vector2){p->pos.x, p->pos.y};
+        break;
+      }
+    }
+  }
+  for (int i = 0; i < MAX_BULLETS; i++) {
+    if (p->bullets[i].active) {
+      p->bullets[i].pos.y -= p->bullets[i].speed;
+      if (p->bullets[i].pos.y < 0) {
+        p->bullets[i].active = false;
+      }
+    }
+  }
 }
 void DrawPlayer(Player *p) {
   DrawTriangle((Vector2){p->pos.x, p->pos.y - 20},
                (Vector2){p->pos.x - 20, p->pos.y + 20},
                (Vector2){p->pos.x + 20, p->pos.y + 20}, YELLOW);
+  for (int i = 0; i < MAX_BULLETS; i++) {
+    if (p->bullets[i].active) {
+      DrawCircleV(p->bullets[i].pos, 5, RED);
+    }
+  }
 }
 void InitPlayer(Player *p) {
   p->pos = (Vector2){300, 700};
   p->speed = 5.0f;
   p->hp = 3;
+  for (int i = 0; i < MAX_BULLETS; i++) {
+    p->bullets[i].active = false;
+    p->bullets[i].speed = 7.0f;
+  }
 }
